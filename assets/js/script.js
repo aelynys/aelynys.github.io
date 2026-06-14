52a7cbdec44e4fab9d14587161dc4098a6dcedca
aelynys <shyan.aelyn@gmail.com>
2026-06-15 03:57:59 +0800

    Add files via upload
    Delete unnecessary js

--- assets/js/script.js
+++ assets/js/script.js
@@ -171,20 +171,3 @@ function isIOS() {
 if (isIOS()) {
   document.documentElement.classList.add('ios');
 }
-
-// // disable right click
-document.addEventListener('contextmenu', e => e.preventDefault());
-
-document.addEventListener('keydown', e => {
-  if (
-    e.key === 'F12' ||
-    (e.ctrlKey && e.key === 'u') ||
-    (e.ctrlKey && e.shiftKey && e.key === 'I') ||  // must be uppercase I
-    (e.ctrlKey && e.shiftKey && e.key === 'J') ||  // must be uppercase J
-    (e.ctrlKey && e.shiftKey && e.key === 'C') ||  // inspector
-    (e.ctrlKey && e.key === 's') ||
-    (e.ctrlKey && e.key === 'S')                   // uppercase too
-  ) {
-    e.preventDefault();
-  }
-});
